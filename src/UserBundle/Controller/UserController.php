5f30491884f52aa7a9812e7f8dd2c77932502d48
mokgosi <mokgosi@gmail.com>
2015-07-23 12:29:45 +0200

    user mng

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -1,9 +1,247 @@
 <?php
+
 namespace UserBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use UserBundle\Form\Type\RegistrationFormType;
+use UserBundle\Entity\User;
+
+/**
+ * Section controller.
+ *
+ * @Route("/user")
+ */
 class UserController extends Controller {
-    
-    public function listAction()  {
-        
+
+    /**
+     * Lists all User entities.
+     *
+     * @Route("/", name="user")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('UserBundle:User')->findAll();
+        dump($users);
+
+        return array(
+            'users' => $users
+        );
     }
+
+    /**
+     * Displays a form to create a new Section entity.
+     *
+     * @Route("/new", name="user_new")
+     * @Method("GET")
+     */
+    public function newAction() {
+        $entity = new User();
+        $form = $this->createCreateForm($entity);
+
+        return $this->render('UserBundle:User:new.html.twig', array(
+                    'entity' => $entity,
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a new Section entity.
+     *
+     * @Route("/", name="user_create")
+     * @Method("POST")
+     */
+    public function createAction(Request $request) {
+        $entity = new User();
+        $form = $this->createCreateForm($entity);
+//
+//        foreach ($form['roles'] as $key => $value) {
+//            $entity->addRole($value);
+//        }
+        
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('user'));
+        }
+
+        return $this->render('UserBundle:User:new.html.twig', array(
+                    'entity' => $entity,
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a User entity.
+     *
+     * @param Section $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(User $entity) {
+        $form = $this->createForm(new RegistrationFormType(), $entity, array(
+            'action' => $this->generateUrl('user_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Finds and displays a User entity.
+     *
+     * @Route("/{id}", name="user_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UserBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing User entity.
+     *
+     * @Route("/{id}/edit", name="user_edit")
+     * @Method("GET")
+     */
+    public function editAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UserBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('UserBundle:User:edit.html.twig', array(
+                    'entity' => $entity,
+                    'form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to edit a User entity.
+     *
+     * @param Section $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(User $entity) {
+        $form = $this->createForm(new RegistrationFormType(), $entity, array(
+            'action' => $this->generateUrl('user_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+
+    /**
+     * Edits an existing User entity.
+     *
+     * @Route("/{id}", name="user_update")
+     * @Method("PUT")
+     */
+    public function updateAction(Request $request, $id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UserBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        
+        foreach ($editForm['roles'] as $key => $value) {
+            $entity->addRole($value);
+        }
+        $editForm = $this->createEditForm($entity);
+        
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('user'));
+        }
+
+        return $this->render('UserBundle:User:edit.html.twig', array(
+                    'entity' => $entity,
+                    'form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a User entity.
+     *
+     * @Route("/{id}", name="user_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id) {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('UserBundle:User')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find User entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('user'));
+    }
+
+    /**
+     * Creates a form to delete a User entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id) {
+        return $this->createFormBuilder()
+                        ->setAction($this->generateUrl('user_delete', array('id' => $id)))
+                        ->setMethod('DELETE')
+                        ->add('submit', 'submit', array('label' => 'Delete'))
+                        ->getForm()
+        ;
+    }
+
 }
--- src/UserBundle/Form/Type/RegistrationFormType.php
+++ src/UserBundle/Form/Type/RegistrationFormType.php
@@ -5,24 +5,31 @@ namespace UserBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class RegistrationFormType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class RegistrationFormType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         // add your custom field
         $builder->add('first_name');
         $builder->add('last_name');
         $builder->add('telephone');
         $builder->add('mobile');
+        $builder->add('roles', 'choice', array(
+            'choices' => array(
+                'ROLE_ADMIN' => 'ROLE_ADMIN',
+                'ROLE_USER' => 'ROLE_USER',
+            ),
+            'multiple' => true,
+            'expanded' => true,
+            'required' => false,
+        ));
     }
 
-    public function getParent()
-    {
+    public function getParent() {
         return 'fos_user_registration';
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'app_user_registration';
     }
+
 }
